7117fb033c2a1b4743abbb4d4e2862cf0ec6e817
haschen12-tamu <157165968+haschen12-tamu@users.noreply.github.com>
2024-02-11 14:34:05 -0600

    Update Project1Final.m

--- Project_1/Project1Final.m
+++ Project_1/Project1Final.m
@@ -1,239 +1,216 @@
-% Project Week 2
-solver_arr = {"ode1", "ode4", "ode45", "ode23tb"};
+solver_arr = {'ode1', 'ode4', 'ode45', 'ode23tb'};
 
-% Fixed Time Step Arrays
-ode1_dT = {};
-ode1_max_error = {};
-ode1_cpu_time = {};
-
-ode4_dT = {};
-ode4_max_error = {};
-ode4_cpu_time = {};
-
-% Variable Time Step Arrays
-ode45_max_error = {};
-ode45_cpu_time = {};
-
-ode23tb_max_error = {};
-ode23tb_cpu_time = {};
+% Initialize arrays to store simulation results
+ode1_results = struct('dT', [], 'max_error', [], 'cpu_time', []);
+ode4_results = struct('dT', [], 'max_error', [], 'cpu_time', []);
+ode45_results = struct('dT', [], 'max_error', [], 'cpu_time', []);
+ode23tb_results = struct('dT', [], 'max_error', [], 'cpu_time', []);
 
 for i = 1:length(solver_arr)
-    if solver_arr{i,1} == "ode1"
-        dT_arr = [0.001, 0.1, 1]; % Fixed Time Step Values [s]
-        A_arr = [0, 100]; % Constant Torque Values [N*m]
-        b_arr = [10, 0.1]; % Damping Coefficient [N*m*s/rad]
-        J1_arr = [100, 0.01]; % Rotational Inertia [kg*m^2]
-        w_0_arr = [10, 0.0]; % Initial Conditions [rad/s]
-        for j = 1:length(dT_arr)
-            for k = 1:length(A_arr)
-                for l = 1:length(b_arr)
-                    for m = 1:length(J1_arr)
-                        for n = 1:length(w_0_arr)
-                            % Initial Conditions
-                            solver = solver_arr{i,1};
-                            dT = dT_arr(j);
-                            A = A_arr(k);
-                            b = b_arr(l);
-                            J1 = J1_arr(m);
-                            F = 0;
-                            w_0 = w_0_arr(n);
-                            isSin = 0; % Boolean for Sine Wave
-
-                            % CPU Time Before
-                            start_time = cputime;
-                            
-                            % Simulate the system
-                            simout = sim("Project1Week2_Github.slx", "Solver", solver, "FixedStep", string(dT));
-                            
-                            % Extract data
-                            W = simout.w.Data;
-                            W_DOT = simout.w_dot.Data;
-                            T = simout.tout;
-                            dT_simout = simout(i,1).SimulationMetadata.ModelInfo.SolverInfo.FixedStepSize;
-                            tau = simout(j,1).tau.Data;
-
-                            % Error Calculation
-                            % theory_w = (tau/b)*(1 - exp(-b*T/J1)) + w_0*exp(-b*T/J1);
-                            % max_error = theory_w - W;
-                            
-                            % CPU Time After
-                            end_time = cputime;
-                            cpu_time = end_time - start_time;
-                            
-                            ode1_dT = cat(1, ode1_dT, dT_simout);
-                            % ode1_max_error = cat(1, ode1_max_error, max_error_simout);
-                            % ode1_cpu_time = cat(1, ode1_cpu_time, cpu_time);
-                            
-                            % Calculate subplot index
-                            %subplot_index = (i - 1) * length(solvers) + j;
-                            
-                            % Plot angular velocity
-                            %subplot(length(dT), length(solvers)*2, subplot_index);
-                            %plot(T, W);
-                            %title(['Solver: ', char(solver), ', Time Step: ', num2str(dT_val)]);
-                            %xlabel('Time [s]');
-                            %ylabel('Angular Velocity [rad/s]');
-                            
-                            % Plot angular acceleration
-                            %subplot(length(dT), length(solvers)*2, subplot_index + length(solvers)*2); 
-                            %plot(T, W_DOT);
-                            %title(['Solver: ', char(solver), ', Time Step: ', num2str(dT_val)]); % Added title
-                            %xlabel('Time [s]');
-                            %ylabel('Angular Acceleration [rad/s^2]');
-                        end
-                    end
-                end
-            end
-        end
-    elseif solver_arr{i,1} == "ode4"
-        dT_arr = [0.001, 0.1, 1]; % Fixed Time Step Values [s]
-        A_arr = [0, 100]; % Constant Torque Values [N*m]
-        b_arr = [10, 0.1]; % Damping Coefficient [N*m*s/rad]
-        J1_arr = [100, 0.01]; % Rotational Inertia [kg*m^2]
-        w_0_arr = [10, 0.0]; % Initial Conditions [rad/s]
-        for j = 1:length(dT_arr)
-            for k = 1:length(A_arr)
-                for l = 1:length(b_arr)
-                    for m = 1:length(J1_arr)
-                        for n = 1:length(w_0_arr)
-                            % Initial Conditions
-                            solver = solver_arr{i,1};
-                            dT = dT_arr(j);
-                            A = A_arr(k);
-                            b = b_arr(l);
-                            J1 = J1_arr(m);
-                            F = 0;
-                            w_0 = w_0_arr(n);
-                            isSin = 0; % Boolean for Sine Wave
-                            
-                            % Simulate the system
-                            simout = sim("Project1Week2_Github.slx", "Solver", solver, "FixedStep", string(dT));
-
-                            % Extract data
-                            W = simout.w.Data;
-                            W_DOT = simout.w_dot.Data;
-                            T = simout.tout;
-                            dT_simout = simout(i,1).SimulationMetadata.ModelInfo.SolverInfo.FixedStepSize;
-                            
-                            % Error Calculation
-                            % theory_w = 
-                            % w_error = theory_w - W
-                            
-                            % CPU Time
-                            % 
-
-                            ode1_dT = cat(1, ode1_dT, dT_simout);
-                            % ode1_max_error.append(w_error)
-                            % ode1_cpu_time.append()
-                            
-                            % Calculate subplot index
-                            %subplot_index = (i - 1) * length(solvers) + j;
-                            
-                            % Plot angular velocity
-                            %subplot(length(dT), length(solvers)*2, subplot_index);
-                            %plot(T, W);
-                            %title(['Solver: ', char(solver), ', Time Step: ', num2str(dT_val)]);
-                            %xlabel('Time [s]');
-                            %ylabel('Angular Velocity [rad/s]');
-                            
-                            % Plot angular acceleration
-                            %subplot(length(dT), length(solvers)*2, subplot_index + length(solvers)*2); 
-                            %plot(T, W_DOT);
-                            %title(['Solver: ', char(solver), ', Time Step: ', num2str(dT_val)]); % Added title
-                            %xlabel('Time [s]');
-                            %ylabel('Angular Acceleration [rad/s^2]');
-                        end
-                    end
-                end
-            end
-        end
+    solver = solver_arr{i};
     
-    elseif solver_arr{i,1} == "ode45"
-        F_arr = [0.1, 100]; % Frequency of Torque Values [rad/s]
+    if strcmp(solver, 'ode1') || strcmp(solver, 'ode4')
+        dT_arr = [0.001, 0.1, 1]; % Fixed Time Step Values [s]
         A_arr = [0, 100]; % Constant Torque Values [N*m]
         b_arr = [10, 0.1]; % Damping Coefficient [N*m*s/rad]
         J1_arr = [100, 0.01]; % Rotational Inertia [kg*m^2]
         w_0_arr = [10, 0.0]; % Initial Conditions [rad/s]
-        for j = 1:length(F_arr)
-            for k = 1:length(A_arr)
-                for l = 1:length(b_arr)
-                    for m = 1:length(J1_arr)
-                        for n = 1:length(w_0_arr)
-                            % Initial Conditions
-                            solver = solver_arr{i,1};
-                            F = F_arr(j);
-                            A = A_arr(k);
-                            b = b_arr(l);
-                            J1 = J1_arr(m);
-                            w_0 = w_0_arr(n);
-                            isSin = 1; % Boolean for Sine Wave
-                            
+        isSin = 0; % Boolean Operator for Sine Function Initialization [False]
+        F = 0; % Frequency [rad/s]
+
+        for dT = dT_arr
+            for A = A_arr
+                for b = b_arr
+                    for J1 = J1_arr
+                        for w_0 = w_0_arr
+                            % Start CPU time
+                            cpu_time_start = cputime;
+
                             % Simulate the system
-                            simout = sim("Project1Week2_Github.slx", "Solver", solver, "FixedStep", string(dT));
+                            simout = sim('Project1Week2_.slx', ...
+                                'Solver', solver, 'FixedStep', num2str(dT));
+
+                            % End CPU time
+                            cpu_time_end = cputime;
+                            cpu_time = cpu_time_end - cpu_time_start;
 
                             % Extract data
                             W = simout.w.Data;
-                            W_DOT = simout.w_dot.Data;
                             T = simout.tout;
-                            dT_simout = simout(i,1).SimulationMetadata.ModelInfo.SolverInfo.FixedStepSize;
-                            
-                            % Error Calculation
-                            % theory_w = 
-                            % w_error = theory_w - W
-                            
-                            % CPU Time
-                            % 
 
-                            ode1_dT = cat(1, ode1_dT, dT_simout);
+                            % Error Calculation
+                            W_len = length(W);
+                            dt = linspace(0, 25, W_len);
+                            theory_w = theory_omega(dt, A, b, w_0, J1, isSin);
+                            theory_w = transpose(theory_w);
+                            max_error = max(abs(W - theory_w));
+                            
+                            % Store results
+                            if strcmp(solver, 'ode1')
+                                ode1_results.dT(end+1) = dT;
+                                ode1_results.cpu_time(end+1) = cpu_time;
+                                ode1_results.max_error(end+1) = max_error;
+                            else
+                                ode4_results.dT(end+1) = dT;
+                                ode4_results.cpu_time(end+1) = cpu_time;
+                                ode4_results.max_error(end+1) = max_error;
+                            end
                         end
                     end
                 end
             end
         end
-    elseif solver_arr{i,1} == "ode23tb"
-        F = [0.1, 100]; % Frequency of Torque Values [rad/s]
+    elseif strcmp(solver, 'ode45') || strcmp(solver, 'ode23tb')
         A_arr = [0, 100]; % Constant Torque Values [N*m]
-        isSin = 1; % Boolean for Sine Wave
         b_arr = [10, 0.1]; % Damping Coefficient [N*m*s/rad]
         J1_arr = [100, 0.01]; % Rotational Inertia [kg*m^2]
         w_0_arr = [10, 0.0]; % Initial Conditions [rad/s]
-        for j = 1:length(F)
-            for k = 1:length(A_arr)
-                for l = 1:length(b_arr)
-                    for m = 1:length(J1_arr)
-                        for n = 1:length(w_0_arr)
-                            % Initial Conditions
-                            solver = solver_arr{i,1};
-                            F = F_arr(j);
-                            A = A_arr(k);
-                            b = b_arr(l);
-                            J1 = J1_arr(m);
-                            w_0 = w_0_arr(n);
-                            isSin = 1; % Boolean for Sine Wave
-                            
+        isSin = 1; % Boolean Operator for Sine Function Initialization [True]
+        F_arr = [0.1, 100]; % Frequency [rad/s]
+
+        for A = A_arr
+            for b = b_arr
+                for J1 = J1_arr
+                    for w_0 = w_0_arr
+                        for F = F_arr
+                            % Start CPU time
+                            cpu_time_start = cputime;
+
                             % Simulate the system
-                            simout = sim("Project1Week2_Github.slx", "Solver", solver, "FixedStep", string(dT));
+                            dT = 0.001;
+                            simout = sim('Project1Week2_.slx', ...
+                                'Solver', solver, 'FixedStep', num2str(dT));
+
+                            % End CPU time
+                            cpu_time_end = cputime;
+                            cpu_time = cpu_time_end - cpu_time_start;
 
                             % Extract data
                             W = simout.w.Data;
-                            W_DOT = simout.w_dot.Data;
                             T = simout.tout;
-                            dT_simout = simout(i,1).SimulationMetadata.ModelInfo.SolverInfo.FixedStepSize;
-                            
-                            % Error Calculation
-                            % theory_w = 
-                            % w_error = theory_w - W
-                            
-                            % CPU Time
-                            % 
 
-                            ode1_dT = cat(1, ode1_dT, dT_simout);
+                            % Error Calculation
+                            W_len = length(W);
+                            dt = linspace(0, 25, W_len);
+                            theory_w = theory_omega(dt, A, b, w_0, J1, isSin);
+                            theory_w = transpose(theory_w);
+                            max_error = max(abs(W - theory_w));
+
+                            % Store results
+                            if strcmp(solver, 'ode45')
+                                ode45_results.dT(end+1) = dT;
+                                ode45_results.cpu_time(end+1) = cpu_time;
+                                ode45_results.max_error(end+1) = max_error;
+                            else
+                                ode23tb_results.dT(end+1) = dT;
+                                ode23tb_results.cpu_time(end+1) = cpu_time;
+                                ode23tb_results.max_error(end+1) = max_error;
+                            end
                         end
                     end
                 end
             end
         end
     else
-        
+        disp(['Solver ', solver, ' not implemented']);
+    end
+end
+
+% Display results
+disp('ode1 results:');
+disp(ode1_results);
+disp('ode4 results:');
+disp(ode4_results);
+disp('ode45 results:');
+disp(ode45_results);
+disp('ode23tb results:');
+disp(ode23tb_results);
+
+% Plot 1: Max Error vs Time Step (Fixed Time Step)
+figure;
+plot(ode1_results.dT, ode1_results.max_error, '-o', 'DisplayName', 'ODE1');
+hold on;
+plot(ode4_results.dT, ode4_results.max_error, '-s', 'DisplayName', 'ODE4');
+xlabel('Time Step [s]');
+ylabel('Max Error');
+title('Max Error vs Time Step (Fixed Time Step Solvers)');
+legend('show');
+grid on;
+
+% Plot 2: CPU Time vs Time Step (Fixed Time Step)
+figure;
+plot(ode1_results.dT, ode1_results.cpu_time, '-o', 'DisplayName', 'ODE1');
+hold on;
+plot(ode4_results.dT, ode4_results.cpu_time, '-s', 'DisplayName', 'ODE4');
+xlabel('Time Step [s]');
+ylabel('CPU Time Taken [s]');
+title('CPU Time vs Time Step (Fixed Time Step Solvers)');
+legend('show');
+grid on;
+
+% Plot 3: Max Simulation Error vs CPU Time Taken for Fixed Time Step solvers
+figure;
+scatter(ode1_results.cpu_time, ode1_results.max_error, 'o', 'DisplayName', 'ODE1');
+hold on;
+scatter(ode4_results.cpu_time, ode4_results.max_error, 's', 'DisplayName', 'ODE4');
+hold on;
+scatter(ode45_results.cpu_time, ode45_results.max_error, 'd', 'DisplayName', 'ODE45');
+hold on;
+scatter(ode23tb_results.cpu_time, ode23tb_results.max_error, '^', 'DisplayName', 'ODE23TB');
+xlabel('CPU Time Taken [s]');
+ylabel('Max Error');
+title('Max Simulation Error vs CPU Time Taken (Fixed Time Step Solvers)');
+legend('show');
+grid on;
+
+% % Plot 4: Max Simulation Error vs Time Step for Variable Time Step solvers
+% figure;
+% plot(ode45_results.dT, ode45_results.max_error, '-d', 'DisplayName', 'ODE45');
+% hold on;
+% plot(ode23tb_results.dT, ode23tb_results.max_error, '-^', 'DisplayName', 'ODE23TB');
+% xlabel('Time Step [s]');
+% ylabel('Max Simulation Error');
+% title('Max Simulation Error vs Time Step (Variable Time Step Solvers)');
+% legend('show');
+% grid on;
+% 
+% % Plot 5: CPU Time Taken vs Time Step for Variable Time Step solvers
+% figure;
+% plot(ode45_results.dT, ode45_results.cpu_time, '-d', 'DisplayName', 'ODE45');
+% hold on;
+% plot(ode23tb_results.dT, ode23tb_results.cpu_time, '-^', 'DisplayName', 'ODE23TB');
+% xlabel('Time Step [s]');
+% ylabel('CPU Time Taken [s]');
+% title('CPU Time Taken vs Time Step (Variable Time Step Solvers)');
+% legend('show');
+% grid on;
+% 
+% % Plot 6: Max Simulation Error vs CPU Time Taken for Variable Time Step solvers
+% figure;
+% scatter(ode45_results.cpu_time, ode45_results.max_error, 'd', 'DisplayName', 'ODE45');
+% hold on;
+% scatter(ode23tb_results.cpu_time, ode23tb_results.max_error, '^', 'DisplayName', 'ODE23TB');
+% xlabel('CPU Time Taken [s]');
+% ylabel('Max Simulation Error');
+% title('Max Simulation Error vs CPU Time Taken (Variable Time Step Solvers)');
+% legend('show');
+% grid on;
+
+% Theory Omega
+function theory_w = theory_omega(dt, tau, b, w_0, J1, isSin)
+    if isSin == 0
+        theory_w = (tau/b)*(1 - exp(-b*dt/J1)) + w_0*exp(-b*dt/J1);
+    else
+        % % Simulate the system
+        % solver = 'ode4';
+        % dT = 0.001;
+        % simout = sim('Project1Week2_.slx', ...
+        %     'Solver', solver, 'FixedStep', num2str(dT));
+        % 
+        % % Extract data
+        % W_ode4 = simout.w.Data;
+        % 
+        W_ode4 = (tau/b)*(1 - exp(-b*dt/J1)) + w_0*exp(-b*dt/J1);
+        theory_w = W_ode4;
     end
 end
