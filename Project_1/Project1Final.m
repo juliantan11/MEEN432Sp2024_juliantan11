0d7476fe2fb3a3a9897d9267a28100ac900bdd3c
haschen12-tamu <157165968+haschen12-tamu@users.noreply.github.com>
2024-02-11 23:58:50 -0600

    Update Project1Final.m

--- Project_1/Project1Final.m
+++ Project_1/Project1Final.m
@@ -10,7 +10,7 @@ ode23tb_results = struct('max_error', [], 'cpu_time', []);
 
 for i = 1:length(solver_arr)
     solver = solver_arr{i};
-    
+
     if strcmp(solver, 'ode1') || strcmp(solver, 'ode4')
         dT_arr = [0.001, 0.1, 1]; % Fixed Time Step Values [s]
         A_arr = [0, 100]; % Constant Torque Values [N*m]
@@ -46,7 +46,7 @@ for i = 1:length(solver_arr)
                             theory_w = theory_omega(dt, A, b, w_0, J1, isSin);
                             theory_w = transpose(theory_w);
                             max_error = max(abs(W - theory_w));
-                            
+
                             % Store results
                             if strcmp(solver, 'ode1')
                                 ode1_results.dT(end+1) = dT;
@@ -162,62 +162,10 @@ title('Max Simulation Error vs CPU Time Taken (Fixed Time Step Solvers)');
 legend('show');
 grid on;
 
-% % Plot 4: Max Simulation Error vs Time Step for Variable Time Step solvers
-% figure;
-% plot(ode45_results.dT, ode45_results.max_error, '-d', 'DisplayName', 'ODE45');
-% hold on;
-% plot(ode23tb_results.dT, ode23tb_results.max_error, '-^', 'DisplayName', 'ODE23TB');
-% xlabel('Time Step [s]');
-% ylabel('Max Simulation Error');
-% title('Max Simulation Error vs Time Step (Variable Time Step Solvers)');
-% legend('show');
-% grid on;
-% 
-% % Plot 5: CPU Time Taken vs Time Step for Variable Time Step solvers
-% figure;
-% plot(ode45_results.dT, ode45_results.cpu_time, '-d', 'DisplayName', 'ODE45');
-% hold on;
-% plot(ode23tb_results.dT, ode23tb_results.cpu_time, '-^', 'DisplayName', 'ODE23TB');
-% xlabel('Time Step [s]');
-% ylabel('CPU Time Taken [s]');
-% title('CPU Time Taken vs Time Step (Variable Time Step Solvers)');
-% legend('show');
-% grid on;
-% 
-% % Plot 6: Max Simulation Error vs CPU Time Taken for Variable Time Step solvers
-% figure;
-% scatter(ode45_results.cpu_time, ode45_results.max_error, 'd', 'DisplayName', 'ODE45');
-% hold on;
-% scatter(ode23tb_results.cpu_time, ode23tb_results.max_error, '^', 'DisplayName', 'ODE23TB');
-% xlabel('CPU Time Taken [s]');
-% ylabel('Max Simulation Error');
-% title('Max Simulation Error vs CPU Time Taken (Variable Time Step Solvers)');
-% legend('show');
-% grid on;
-
-% Theory Omega
-function theory_w = theory_omega(dt, tau, b, w_0, J1, isSin)
-    if isSin == 0
-        theory_w = (tau/b)*(1 - exp(-b*dt/J1)) + w_0*exp(-b*dt/J1);
-    else
-        % % Simulate the system
-        solver = 'ode4';
-        dT = 0.001;
-        simout = sim('Project1Part1_.slx', ...
-            'Solver', solver, 'FixedStep', num2str(dT));
-
-        % Extract data
-        W_ode4 = simout.w.Data;
-
-        W_ode4 = (tau/b)*(1 - exp(-b*dt/J1)) + w_0*exp(-b*dt/J1);
-        theory_w = W_ode4;
-    end
-end
-
 
 % Part 2
 
-solver_arr = {'ode1', 'ode4', 'ode45'};
+solver2_arr = {'ode1', 'ode4', 'ode45'};
 
 % Initialize arrays to store simulation results
 ode1_results = struct('omega_shaft', [], 'cpu_time', []);
@@ -230,10 +178,10 @@ for i = 1:length(solver_arr)
     if strcmp(solver, 'ode1') || strcmp(solver, 'ode4')
         dT_arr = [0.1, 1]; % Fixed Time Step Values [s]
         A_arr = [1, 100]; % Constant Torque Values [N*m]
-        b1_arr = [1]; % Damping Coefficient [N*m*s/rad]
-        J1_arr = [100]; % Rotational Inertia [kg*m^2]
-        b2_arr = [1]; % Damping Coefficient [N*m*s/rad]
-        J2_arr = [1]; % Rotational Inertia [kg*m^2]
+        b1_arr = 1; % Damping Coefficient [N*m*s/rad]
+        J1_arr = 100; % Rotational Inertia [kg*m^2]
+        b2_arr = 1; % Damping Coefficient [N*m*s/rad]
+        J2_arr = 1; % Rotational Inertia [kg*m^2]
         k_arr = [10, 100, 1000]; % Rotational Inertia [kg*m^2]
         w_0_arr = [10, 0.0]; % Initial Conditions [rad/s]
 
@@ -257,18 +205,14 @@ for i = 1:length(solver_arr)
                                         cpu_time = cpu_time_end - cpu_time_start;
     
                                         % Extract data
-                                        W = simout.w.Data;
+                                        out_total = simout.out_total.Data;
                                         T = simout.tout;
     
                                         % Store results
                                         if strcmp(solver, 'ode1')
-                                            ode1_results.dT(end+1) = dT;
                                             ode1_results.cpu_time(end+1) = cpu_time;
-                                            ode1_results.max_error(end+1) = max_error;
                                         else
-                                            ode4_results.dT(end+1) = dT;
                                             ode4_results.cpu_time(end+1) = cpu_time;
-                                            ode4_results.max_error(end+1) = max_error;
                                         end
                                     end
                                 end
@@ -280,10 +224,10 @@ for i = 1:length(solver_arr)
         end
     elseif strcmp(solver, 'ode45')
         A_arr = [1, 100]; % Constant Torque Values [N*m]
-        b1_arr = [1]; % Damping Coefficient [N*m*s/rad]
-        J1_arr = [100]; % Rotational Inertia [kg*m^2]
-        b2_arr = [1]; % Damping Coefficient [N*m*s/rad]
-        J2_arr = [1]; % Rotational Inertia [kg*m^2]
+        b1_arr = 1; % Damping Coefficient [N*m*s/rad]
+        J1_arr = 100; % Rotational Inertia [kg*m^2]
+        b2_arr = 1; % Damping Coefficient [N*m*s/rad]
+        J2_arr = 1; % Rotational Inertia [kg*m^2]
         k_arr = [10, 100, 1000]; % Rotational Inertia [kg*m^2]
         w_0_arr = [10, 0.0]; % Initial Conditions [rad/s]
         
@@ -304,12 +248,11 @@ for i = 1:length(solver_arr)
                             cpu_time = cpu_time_end - cpu_time_start;
     
                             % Extract data
-                            W = simout.w.Data;
+                            out_total = simout.out_total.Data;
                             T = simout.tout;
     
                             % Store results
                             ode45_results.cpu_time(end+1) = cpu_time;
-                            ode45_results.max_error(end+1) = max_error;
                         end
                     end
                 end
@@ -321,10 +264,10 @@ for i = 1:length(solver_arr)
 end
 
 figure;
-scatter(out.total,time, '-o', 'DisplayName', 'k=10')
+plot(out_total,cpu_time, '-o', 'DisplayName', 'k=10')
 hold on;
-scatter(out.total,time, 's', 'DiplayName', 'k=100' )
-scatter(out.total, time, 'd', 'DisplayName', 'k=1000')
+plot(out_total,cpu_time, 's', 'DiplayName', 'k=100' )
+plot(out_total, cpu_time, 'd', 'DisplayName', 'k=1000')
 xlabel('Time')
 ylabel('Shaft Speed')
 title('Shaft Speed vs. Time')
@@ -332,11 +275,30 @@ legend('show')
 grid on;
 
 figure;
-scatter(out.total,time, '-o', 'DisplayName', 'torque=1')
+plot(out_total,cpu_time, '-o', 'DisplayName', 'torque=1')
 hold on;
-scatter(out.total,time, 's', 'DiplayName', 'torque=100' )
+plot(out_total,cpu_time, 's', 'DiplayName', 'torque=100' )
 xlabel('Time')
 ylabel('Shaft Speed')
 title('Shaft Speed vs. Time')
 legend('show')
 grid on;
+
+% Theory Omega
+function theory_w = theory_omega(dt, tau, b, w_0, J1, isSin)
+    if isSin == 0
+        theory_w = (tau/b)*(1 - exp(-b*dt/J1)) + w_0*exp(-b*dt/J1);
+    else
+        % % Simulate the system
+        solver = 'ode4';
+        dT = 0.001;
+        simout = sim('Project1Part1_.slx', ...
+            'Solver', solver, 'FixedStep', num2str(dT));
+
+        % Extract data
+        W_ode4 = simout.w.Data;
+
+        W_ode4 = (tau/b)*(1 - exp(-b*dt/J1)) + w_0*exp(-b*dt/J1);
+        theory_w = W_ode4;
+    end
+end
